1320d57ebd9090a4d2f62ca857ef5197a1c014a8
ez-devstack <ez.devstack@gmail.com>
2025-02-23 21:53:07 -0600

    pygame sreen setup

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import pygame
+from constants import *
 
 def main():
-    print("Starting Asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.fill((0,0,0))
+    pygame.display.flip()
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
 
 if __name__ == "__main__":
     main()
