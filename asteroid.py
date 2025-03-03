e174b1aeb26c84d43f48cd55d5f4c24cbd1b5b5b
ez-devstack <ez.devstack@gmail.com>
2025-03-02 22:02:52 -0600

    finish base game setup

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from circleshape import CircleShape
 from constants import *
 
@@ -12,3 +13,13 @@ class Asteroid(CircleShape):
     def update(self, dt):
         self.position += self.velocity * dt
     
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            random_angle = random.uniform(20, 50)
+            asteroid_one = Asteroid(self.position.x, self.position.y, (self.radius - ASTEROID_MIN_RADIUS))
+            asteroid_two = Asteroid(self.position.x, self.position.y, (self.radius - ASTEROID_MIN_RADIUS))
+            asteroid_one.velocity = self.velocity.rotate(random_angle) * 1.2
+            asteroid_two.velocity = self.velocity.rotate(-random_angle) * 1.2
--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
         
         # in the player class
     def triangle(self):
@@ -25,7 +26,8 @@ class Player(CircleShape):
     
     def update(self, dt):
         keys = pygame.key.get_pressed()
-
+        self.timer -= dt
+        
         if keys[pygame.K_a]:
             self.rotate(-dt)
         if keys[pygame.K_d]:
@@ -36,11 +38,14 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_SPACE]:
             self.shoot()
-            
+        
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
         
     def shoot(self):
+        if self.timer > 0:
+            return
+        self.timer = PLAYER_SHOOT_COOLDOWN
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
